0f89a1d92810295c4ca9611055e445b63daafc51
Christopher-Kaiser <christophermsindo97@gmail.com>
2025-09-01 06:31:06 -0400

    fixed some syntax and mistakes on lines 43 and 55

--- sentiment/index.js
+++ sentiment/index.js
@@ -40,7 +40,7 @@ app.post('/sentiment', async (req, res) => {
         // Task 5: set sentiment to negative or positive based on score rules
         if(analysisResult < 0) {
             sentiment = "negative";
-        } else if(analysisResultc> 0.33) {
+        } else if(analysisResult > 0.33) {
             sentiment = "positive";
         }
 
@@ -52,7 +52,7 @@ app.post('/sentiment', async (req, res) => {
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        res.status(500).json({'message': 'Error performing sentiment analysis'});
+        res.status(500).json({ message: 'Error performing sentiment analysis'});
     }
 });
 
