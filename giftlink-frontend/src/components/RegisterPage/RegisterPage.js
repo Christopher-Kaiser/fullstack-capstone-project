f64e402042d79231d7caa0901b61dc28250634da
Christopher-Kaiser <christophermsindo97@gmail.com>
2025-09-03 05:03:53 -0400

    finished implementation of backend in register component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,22 +1,64 @@
 import React, { useState } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 import './RegisterPage.css';
 
 function RegisterPage() {
 
-    //insert code here to create useState hook variables for firstName, lastName, email, password
+    // create useState hook variables for firstName, lastName, email, password
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    // insert code here to create handleRegister function and include console.log
+    const [showerr, setShowerr] = useState(''); // state for error message
+
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
+    // create handleRegister function and include console.log
     const handleRegister = async () => {
         try {
-        //Step 1: Implement API call
-        //Step 2: Access data, login, set the AuthContext and set user details
-        } catch {
-            
+            // Implement API call
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password
+                })
+            })
+
+            // Access data coming from fetch API
+            const json = await response.json();
+            console.log('json data', json);
+            console.log('er', json.error);
+
+            // Set user details
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', firstName);
+                sessionStorage.setItem('email', json.email);
+
+                // Set the state of user to logged in using the `useAppContext`
+                setIsLoggedIn(true);
+
+                // Navigate to the MainPage after logging in
+                navigate('/app');
+            }
+
+            // Set an error message if the registration fails
+            if (json.error) {
+                setShowerr(json.error);
+            }
+        } catch (e) {
+            console.log("Error fetching details: " + e.message);
         }
     }
 
@@ -27,9 +69,9 @@ function RegisterPage() {
                     <div className="register-card p-4 border rounded">
                         <h2 className="text-center mb-4 font-weight-bold">Register</h2>
 
-                        {/* insert code here to create input elements for all the variables - firstName, lastName, email, password */}
+                        {/* create input elements for all the variables - firstName, lastName, email, password */}
                         <div className="mb-4">
-                            <label htmlFor="firstName" className="form label"> First Name</label>
+                            <label htmlFor="firstName" className="form-label"> First Name</label>
                             <input
                                 id="firstName"
                                 type="text"
@@ -40,7 +82,7 @@ function RegisterPage() {
                             />
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="lastName" className="form label"> Last Name</label>
+                            <label htmlFor="lastName" className="form-label"> Last Name</label>
                             <input
                                 id="lastName"
                                 type="text"
@@ -51,7 +93,7 @@ function RegisterPage() {
                             />
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="email" className="form label"> Email</label>
+                            <label htmlFor="email" className="form-label"> Email</label>
                             <input
                                 id="email"
                                 type="text"
@@ -60,9 +102,11 @@ function RegisterPage() {
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
+                            {/* Display error message to end user */}
+                            <div className="text-danger">{showerr}</div>
                         </div>
                         <div className="mb-4">
-                            <label htmlFor="password" className="form label"> Password</label>
+                            <label htmlFor="password" className="form-label"> Password</label>
                             <input
                                 id="password"
                                 type="password"
@@ -73,10 +117,10 @@ function RegisterPage() {
                             />
                         </div>
 
-                        {/* insert code here to create a button that performs the `handleRegister` function on click */}
+                        {/* create a button that performs the `handleRegister` function on click */}
                         <button className="btn btn-primary mb-3" onClick={handleRegister}>Register</button>
 
-                            <p className="mt-4 text-center">
+                        <p className="mt-4 text-center">
                             Already a member? <a href="/app/login" className="text-primary">Login</a>
                         </p>
                     </div>
