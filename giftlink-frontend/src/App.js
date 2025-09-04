b832814db9760fbcfee6c8d32c93fbaf56100bc7
Christopher-Kaiser <christophermsindo97@gmail.com>
2025-09-04 03:10:44 -0400

    implemented backend in the profile component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,6 +5,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -15,13 +16,14 @@ function App() {
         <>
             <Navbar/>
             <Routes>
-            {/* the final code will not pass the products to every page, but each page will call the server API */}
-            <Route path="/" element={<MainPage />} />
-            <Route path="/app" element={<MainPage />} />
-            <Route path="/app/register" element={<RegisterPage />} />
-            <Route path="/app/login" element={<LoginPage />} />
-            <Route path="/app/product/:productId" element={<DetailsPage/>} />
-            <Route path="/app/search" element={<SearchPage/>} />
+                {/* the final code will not pass the products to every page, but each page will call the server API */}
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage/>} />
+                <Route path="/app/search" element={<SearchPage/>} />
+                <Route path="/app/profile" element={<Profile/>} />
             </Routes>
         </>
     );
